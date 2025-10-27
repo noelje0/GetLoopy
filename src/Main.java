3f7a27cb76750b92b526e6076fa0e63aaac3d195
JJ <noelje@mail.uc.edu>
2025-10-26 23:33:46 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// java
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
+        boolean playAgain;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            int roll = 1;
+
+            System.out.printf("%5s\t%8s\t%8s\t%8s\t%6s%n", "Roll", "Die1", "Die2", "Die3", "Sum");
+            System.out.println("----------------------------------------------------------");
+
+            while (true) {
+                int d1 = rnd.nextInt(6) + 1;
+                int d2 = rnd.nextInt(6) + 1;
+                int d3 = rnd.nextInt(6) + 1;
+                int sum = d1 + d2 + d3;
+
+                System.out.printf("%5d\t%8d\t%8d\t%8d\t%6d%n", roll, d1, d2, d3, sum);
+
+                if (d1 == d2 && d2 == d3) {
+                    System.out.println("You win!");
+                    break;
+                }
+                roll++;
+            }
+
+            System.out.print("Play again? (y/n): ");
+            String response = scanner.nextLine().trim().toLowerCase();
+            playAgain = response.startsWith("y");
+            System.out.println();
+        } while (playAgain);
+
+        scanner.close();
     }
 }
